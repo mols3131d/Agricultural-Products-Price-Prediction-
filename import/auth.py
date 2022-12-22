c1cdb8afe3d4d69b302508793dfd252e7fd1981f
Sankamita3131 <sankamita3131@gmail.com>
2022-12-23 02:20:34 +0900

    수정

--- import/auth.py
+++ import/auth.py
@@ -1,3 +1,3 @@
 auth = {
-    "data_co_kr" : r""""b8m%2Fpx7AI1NzkWN%2FlcRj9uXgO56otbz7ZIBfXcplZtC7kmPv1qdNbWD9koAjSQp6t%2Beo2JJPcn%2BFiGNuklo98A%3D%3D"""
+    "data_co_kr" : r"""b8m/px7AI1NzkWN/lcRj9uXgO56otbz7ZIBfXcplZtC7kmPv1qdNbWD9koAjSQp6t+eo2JJPcn+FiGNuklo98A=="""
 }
