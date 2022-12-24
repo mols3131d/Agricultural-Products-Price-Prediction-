6dbc644327861c3b43ad2a494f465ac3cffd3b54
Sankamita3131 <sankamita3131@gmail.com>
2022-12-24 20:46:52 +0900

    갱신

--- import/auth.py
+++ import/auth.py
@@ -1,3 +1,4 @@
 authkey = {
-    "AgriWeather" : r"""b8m/px7AI1NzkWN/lcRj9uXgO56otbz7ZIBfXcplZtC7kmPv1qdNbWD9koAjSQp6t+eo2JJPcn+FiGNuklo98A=="""
+    "AgriWeather_enc" : r"""b8m%2Fpx7AI1NzkWN%2FlcRj9uXgO56otbz7ZIBfXcplZtC7kmPv1qdNbWD9koAjSQp6t%2Beo2JJPcn%2BFiGNuklo98A%3D%3D""",
+    "AgriWeather_dec" : r"""l8+rL+9kwgF5FUm0JLsaes0UoD1S/+reZ3GZFYmQ4EgvE2drQq4gWIBZi2KSbGD+G+2qhyB+vrXBxEWHEIVBdg=="""
 }
