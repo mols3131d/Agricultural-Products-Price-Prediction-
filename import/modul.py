f47d2db2662f8a4ea43086023294b3a74605c534
Sankamita3131 <sankamita3131@gmail.com>
2022-12-27 15:21:16 +0900

    갱신

--- import/modul.py
+++ import/modul.py
@@ -36,3 +36,54 @@ def reduce_mem_usage(df, verbose=True):
             )
         )
     return df
+
+
+def envPath(dict_path=None):
+    import platform
+    from pprint import pprint
+
+    global root_path
+    global data_path
+
+    dict_path_default = {
+        "Windows": {"root_path": "../", "data_path": "data"},
+        "Google_Colab": {"root_path": "../gdrive/Shareddrives/Agricultural Products Price Prediction/", "data_path": "data"},
+        "Kaggle_notebook": {"root_path": "..", "data_path": "input"},
+    }
+    # 추후에 dict_path에 Kaggle_notebook이 없으면 추가하는 코드 추가 요망
+    if dict_path != None:
+        for k, v in dict_path.items():
+            dict_path_default[k] = v
+
+    dict_path = dict_path_default
+
+
+    env = platform.uname().system
+
+    if env == "Windows":
+        env = env
+
+    elif env == "Linux":
+        import sys
+
+        # Google_Colab
+        if "google.colab" in sys.modules:
+            env = "Google_Colab"
+            from google.colab import drive
+
+            drive.mount("/gdrive", force_remount=True)
+
+        # Kaggle_notebook
+        else:
+            import os
+
+            if list(os.walk("/kaggle/"))[0][1] == ["lib", "input", "working"]:
+                env = "Kaggle_notebook"
+
+    root_path = dict_path[env]["root_path"]
+    data_path = f"{root_path}/{dict_path[env]['data_path']}"
+    print("▣ env :", env)
+    print()
+    print("▣ platform.uname()", "\n", platform.uname(), sep="")
+    print()
+    print("▣ data_path", "\n", data_path, sep="")
