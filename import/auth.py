6a8ca970f7555cabed534a3a8ce756f1497f7372
Sankamita3131 <sankamita3131@gmail.com>
2022-12-28 16:55:48 +0900

    c
    c

--- import/auth.py
+++ import/auth.py
@@ -1,6 +1,8 @@
 authkey = {
-    "AgriWeather_enc": r"""b8m%2Fpx7AI1NzkWN%2FlcRj9uXgO56otbz7ZIBfXcplZtC7kmPv1qdNbWD9koAjSQp6t%2Beo2JJPcn%2BFiGNuklo98A%3D%3D""",
-    "AgriWeather_dec": r"""l8+rL+9kwgF5FUm0JLsaes0UoD1S/+reZ3GZFYmQ4EgvE2drQq4gWIBZi2KSbGD+G+2qhyB+vrXBxEWHEIVBdg==""",
+    "WeatherObsrInfo_enc": r"""b8m%2Fpx7AI1NzkWN%2FlcRj9uXgO56otbz7ZIBfXcplZtC7kmPv1qdNbWD9koAjSQp6t%2Beo2JJPcn%2BFiGNuklo98A%3D%3D""",
+    "WeatherObsrInfo_dec": r"""l8+rL+9kwgF5FUm0JLsaes0UoD1S/+reZ3GZFYmQ4EgvE2drQq4gWIBZi2KSbGD+G+2qhyB+vrXBxEWHEIVBdg==""",
     "FmlandWthrInfo_enc": r"""b8m%2Fpx7AI1NzkWN%2FlcRj9uXgO56otbz7ZIBfXcplZtC7kmPv1qdNbWD9koAjSQp6t%2Beo2JJPcn%2BFiGNuklo98A%3D%3D""",
     "FmlandWthrInfo_dec": r"""b8m/px7AI1NzkWN/lcRj9uXgO56otbz7ZIBfXcplZtC7kmPv1qdNbWD9koAjSQp6t+eo2JJPcn+FiGNuklo98A==""",
+    "FmlandWthrInfo_enc_2": r"""l8%2BrL%2B9kwgF5FUm0JLsaes0UoD1S%2F%2BreZ3GZFYmQ4EgvE2drQq4gWIBZi2KSbGD%2BG%2B2qhyB%2BvrXBxEWHEIVBdg%3D%3D""",
+    "FmlandWthrInfo_dec_2": r"""l8+rL+9kwgF5FUm0JLsaes0UoD1S/+reZ3GZFYmQ4EgvE2drQq4gWIBZi2KSbGD+G+2qhyB+vrXBxEWHEIVBdg==""",
 }
